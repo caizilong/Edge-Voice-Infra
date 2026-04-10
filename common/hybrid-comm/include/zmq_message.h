396db5ddbcfa419b223c87818ad3cf6ad7c7fa2e
caizilong <2431764598@qq.com>
2026-04-10 22:10:06 +0800

    zmq message

---
+++ common/hybrid-comm/include/zmq_message.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "zmq.h"
+#include <memory>
+#include <string>
+
+namespace StackFlows {
+class ZmqMessage {
+private:
+    zmq_msg_t msg;
+
+public:
+    ZmqMessage();
+    ~ZmqMessage();
+
+    std::shared_ptr<std::string> get_string();
+    std::string string();
+    void *data();
+    size_t size();
+    zmq_msg_t *get();
+
+    std::string get_param(int index, const std::string &idata = "");
+    std::string set_param(std::string param0, std::string param1);
+};
+}  // namespace StackFlows
