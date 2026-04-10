b2a0ac80d69d06d8051378c251af88b4956865d2
caizilong <2431764598@qq.com>
2026-04-10 22:40:33 +0800

    zmq

---
+++ common/hybrid-comm/include/zmq_endpoint.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <libzmq/zmq.h>
+#include <unistd.h>
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+#include "zmq_message.h"
+
+namespace StackFlows {
+class ZmqEndpoint {
+public:
+    
+};
+}  // namespace StackFlows
