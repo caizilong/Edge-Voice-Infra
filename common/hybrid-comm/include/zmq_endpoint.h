f2e8217f0943b49b03e5e4e9f9a0dccbb23ff019
caizilong <2431764598@qq.com>
2026-04-14 22:10:09 +0800

    network

--- common/hybrid-comm/include/zmq_endpoint.h
+++ common/hybrid-comm/include/zmq_endpoint.h
@@ -15,6 +15,29 @@
 namespace StackFlows {
 class ZmqEndpoint {
 public:
-    
+    using rpc_callback_fun =
+            std::function<std::string(ZmqEndpoint*, const std::shared_ptr<ZmqMessage>&)>;
+    using msg_callback_fun = std::function<void(ZmqEndpoint*, const std::shared_ptr<ZmqMessage>&)>;
+
+public:
+    const int rpc_url_head_length = 6;
+    std::string rpc_url_head_ = "ipc:///tmp/rpc.";
+    void* zmq_ctx_;
+    void* zmq_socket_;
+    std::unordered_map<std::string, rpc_callback_fun> zmq_fun_;
+    std::mutex zmq_fun_mtx_;
+    std::atomic<bool> flage_;
+    std::unique_ptr<std::thread> zmq_thread_;
+    int mode_;
+    std::string rpc_server_;
+    std::string zmq_url_;
+    int timeout_;
+
+public:
+    ZmqEndpoint(const std::string& url);
+    ~ZmqEndpoint();
+
+    void register_rpc_action(const std::string& action, const rpc_callback_fun& callback);
+    void register_msg_action(const std::string& action, const msg_callback_fun& callback);
 };
 }  // namespace StackFlows
---
+++ common/network/include/util.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace network {
+pid_t getPid();
+pid_t getThreadId();
+int64_t getNowMs();
+int32_t getInt32FormNetByte(const char *buf);
+} // namespace network
