c7a240d5fa0d5abdcd2558579543f1728d82046b
caizilong <2431764598@qq.com>
2026-04-22 22:40:46 +0800

    buffer

---
+++ common/network/src/buffer.cc
@@ -0,0 +1,34 @@
+#include "buffer.h"
+
+#include <errno.h>
+#include <sys/uio.h>
+
+#include "SockersOps.h"
+
+namespace network {
+    const char Buffer::kCRLF[] = "\r\n";
+
+    const size_t Buffer::kCheapPrepend;
+    const size_t Buffer::kInitialSize;
+
+    ssize_t Buffer::readFd(int fd, int *savedErrno) {
+        char extrabuf[1024 * 1024];
+        struct iovec vec[2];
+        const size_t writable = writableBytes();
+        vec[0].iov_base = begin() + writerIndex_;
+        vec[0].iov_len = writable;
+        vec[1].iov_base = extrabuf;
+        vec[1].iov_len = sizeof extrabuf;
+        const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
+        const ssize_t n = sockets::readv(fd, vec, iovcnt);
+        if (n < 0) {
+            *savedErrno = errno;
+        } else if (n <= writable) {
+            writerIndex_ += n;
+        } else {
+            writerIndex_ = buffer_.size();
+            append(extrabuf, n - writable);
+        }
+        return n;
+    }
+}
